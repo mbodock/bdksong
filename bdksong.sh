a77dfda959ddd326cb232addd05d49e4deeb12b3
Mbodock (Marcus Vinicius) <mbodock@gmail.com>
2014-05-04 15:56:34 -0300

    Commit inicial

---
+++ bdksong.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Busca e executa uma serie de musicas no diretório atual.
+# A partir do nome do arquivo.
+#
+# ex:
+# bdksong Adele
+#
+# Author Mbodock (mbodock@gmail.com)
+#
+# Versão 0.22
+#
+# Requisitos: mplayer
+
+# Testa as dependências: mplayer
+if type mplayer &>/dev/null; then
+    true
+else
+    echo "O bdksong exige o seguinte programa:"
+    echo " -mplayer"
+    echo "Gostaria de instalá-lo? <s/n>"
+    read option
+
+    if [ "$option" = "s" ]
+    then
+        sudo apt-get install mplayer
+        clear
+    else
+        exit 0
+    fi
+fi
+
+# Configurando o Path
+file_cfg="$HOME/.bdksong.cfg"
+if [ -e "$file_cfg" ]
+then
+    music_path=$(cat $file_cfg)
+
+else
+    echo "Onde você armazena suas músicas? (por favor use o caminho completo)"
+    read music_path
+    echo $music_path > $file_cfg
+    echo "Configurações salvas em $file_cfg"
+    echo "Pronto!"
+fi
+
+cd $music_path
+list="/tmp/bdklist.list"
+echo "$(find $(pwd) | grep -i "$1" | while read x; do echo "$x"; done)" > $list
+
+mplayer -playlist $list
+
+rm $list
